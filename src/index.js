012fdca9ef7aad975d0a212a1034f69480194de2
Thaislaa <thaisladaveiga@gmail.com>
2025-09-22 12:52:39 -0300

    Criacao do servidor Express

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-console.log("Hello!");
+import express from "express";
+import cors from "cors";
+import * as dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.get("/hello", (req, res) => {
+   res.send("Hello!");
+});
+
+const porta = process.env.PORT;
+app.listen(porta, () => {
+    console.log("O servidor está rodando na porta " + porta);
+});
+
+
